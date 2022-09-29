111b585f62c52405e737edd8d1df3faf0ca8ba67
boriscetkovic5 <boriscetkovic.me@gmail.com>
2022-09-30 00:28:43 +0200

    added new pages & scripts

--- app/index.js
+++ app/index.js
@@ -1,3 +1,21 @@
+// ----- Book Modal tabs -----//
+const bookModalTabs = document.querySelectorAll('.book-modal-main__toggle');
+const bookModalContent = document.querySelectorAll('.book-modal-main__content');
+bookModalTabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+        bookModalContent.forEach((content) => {
+            content.classList.remove('is-active');
+        });
+
+        bookModalTabs.forEach((tab) => {
+            tab.classList.remove('is-active');
+        });
+
+        bookModalContent[index].classList.add('is-active');
+        bookModalTabs[index].classList.add('is-active');
+    });
+});
+
 // ----- Single Student tabs -----//
 const singleStudentTabs = document.querySelectorAll('.single-student__toggle ');
 const singleStudentContent = document.querySelectorAll('.single-student__content');
@@ -17,24 +35,6 @@ singleStudentTabs.forEach((tab, index) => {
     });
 });
 
-
-// ----- Book Modal tabs -----//
-const bookModalTabs = document.querySelectorAll('.book-modal-main__toggle');
-const bookModalContent = document.querySelectorAll('.book-modal-main__content');
-bookModalTabs.forEach((tab, index) => {
-    tab.addEventListener('click', () => {
-        bookModalContent.forEach((content) => {
-            content.classList.remove('is-active');
-        });
-
-        bookModalTabs.forEach((tab) => {
-            tab.classList.remove('is-active');
-        });
-
-        bookModalContent[index].classList.add('is-active');
-        bookModalTabs[index].classList.add('is-active');
-    });
-});
 
 
 // ----- Show all activities ----- //
@@ -67,56 +67,4 @@ new Chart(barChart, {
 });
 
 
-// ----- Modals ----- //
 
-
-// Single User Modal
-const singleUserModal = document.querySelector('.single-user');
-const singleUserModalBtn = document.querySelector('.single-user-modal-open');
-singleUserModalBtn.addEventListener('click', () => {
-    singleUserModal.style.display = 'block';
-});
-
-// Single Student Modal
-const singleStudentModal = document.querySelector('.single-student');
-const singleStudentModalBtn = document.querySelector('.single-student-modal-open');
-singleStudentModalBtn.addEventListener('click', () => {
-    singleStudentModal.style.display = 'block';
-});
-
-// Book Modal
-const bookModal = document.querySelector('.book-modal');
-const bookModalBtn = document.querySelector('.book-modal-open');
-bookModalBtn.addEventListener('click', () => {
-    bookModal.style.display = 'block';
-});
-
-
-// Close Modal
-const btnCancel = document.querySelectorAll('.btn-cancel');
-const btnSubmit = document.querySelectorAll('.btn-save');
-const navLinks = document.querySelectorAll('.nav-links');
-
-function closeModals() {
-    singleUserModal.style.display = 'none';
-    singleStudentModal.style.display = 'none';
-    bookModal.style.display = 'none';
-}
-
-btnCancel.forEach(cancel => {
-    cancel.addEventListener('click', () => {
-        closeModals();
-    });
-});
-
-btnSubmit.forEach(submit => {
-    submit.addEventListener('click', () => {
-        closeModals();
-    });
-});
-
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        closeModals();
-    });
-});
